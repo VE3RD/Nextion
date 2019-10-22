381a22a7a9b3ca8f5100f2d550a0fb0f5cbeacf6
root <root@pi-star>
2019-10-22 13:45:39 -0400

    Crosmodes addition

---
+++ clearmodes.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+############################################################
+#  Clear Modes in /etc/mmdvmhost                           #
+#  Also disable corresponding network                       #
+#  KF6S                                        09-14-2019  #
+############################################################
+set -o errexit
+set -o pipefail
+# Clear all six modes and set each one to either 0 or 1
+
+        sudo sed -i '/\[D-Star\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+        sudo sed -i '/\[D-Star Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+
+# Turn on DMR Default
+        sudo sed -i '/\[DMR\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+	sudo sed -i '/\[DMR Network\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+
+        sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+	sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+
+        sudo sed -i '/\[P25\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+	sudo sed -i '/\[P25 Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+
+        sudo sed -i '/\[NXDN\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+	sudo sed -i '/\[NXDN Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+
+        sudo sed -i '/\[POCSAG\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+	sudo sed -i '/\[POCSAG Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+
+        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/dmr2ysf
+        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/dmr2nxdn
+        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/ysf2dmr
+        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/ysf2nxdn
+        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/ysf2p25
+
+  sudo /usr/local/sbin/mmdvmhost.service restart
+
--- setcrossmode.sh
+++ setcrossmode.sh
@@ -7,56 +7,105 @@
 ############################################################
 set -o errexit
 set -o pipefail
-# Check all six modes and set each one to either 0 or 1
-
+# Check all five cross modes and set each one to either 0 or 1
+#Clear all Main Modes
+sudo /usr/local/etc/Nextion_Support/clearmodes.sh
 if [ -z "$1" ]; then
         exit
         else
-        if [ "$1" = 7 ]; then sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"$2"'' /etc/dmr2ysf
-			 sudo sed -i '/\[DMR Network 3\]/!b;n;cEnabled='"$2"'' /etc/dmrgateway
+        if [ "$1" = 7 ]; then 
+           #  sudo /usr/local/sbin/dmr2ysf.service stop
              if [ "$2"  = 1 ]; then  
+ 		sudo sed -i '/\[DMR\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+	         sudo sed -i '/\[DMR Network 3\]/!b;n;cEnabled='"1"'' /etc/dmrgateway
+ 	     	 sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"1"'' /etc/dmr2ysf
 		 sudo sed  -i 's/62031/62033/g' /etc/mmdvmhost
 		 sudo sed  -i 's/tgif.network/127.0.0.2/g' /etc/mmdvmhost
 		 sudo sed  -i 's/passw0rd/none/g' /etc/mmdvmhost
-	fi
-
-             if [ "$2" = 0 ]; then 
+    	 	 sudo /usr/local/sbin/dmr2ysf.service restart
+	     fi  
+	     if [ "$2" != 1 ]; then 
+	 #        sudo sed -i '/\[DMR Network 3\]/!b;n;cEnabled='"0"'' /etc/dmrgateway
+	         sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/dmr2ysf
 		 sudo sed  -i 's/62033/62031/g' /etc/mmdvmhost
 		 sudo sed  -i 's/127.0.0.2/tgif.network/g' /etc/mmdvmhost
 		 sudo sed  -i 's/none/passw0rd/g' /etc/mmdvmhost
              fi
- 		sudo /usr/local/sbin/dmr2ysf.service restart
- 		sudo /usr/local/sbin/mmdvmhost.service restart
         fi
 
-        if [ "$1" = 8 ]; then sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"$2"'' /etc/dmr2nxdn
+        if [ "$1" = 8 ]; then 
+ 	    # sudo /usr/local/sbin/dmr2nxdn.service  stop
              if [ "$2"  = 1 ]; then  
+		 sudo sed -i '/\[DMR Network 3\]/!b;n;cEnabled='"1"'' /etc/dmrgateway
+ 		sudo sed -i '/\[DMR\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+ 	         sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"1"'' /etc/dmr2nxdn 
 		 sudo sed  -i 's/62031/62035/g' /etc/mmdvmhost
 		 sudo sed  -i 's/tgif.network/127.0.0.3/g' /etc/mmdvmhost
 		 sudo sed  -i 's/passw0rd/none/g' /etc/mmdvmhost
-	fi
-
-             if [ "$2" = 0 ]; then 
+	     	 sudo /usr/local/sbin/dmr2nxdn.service restart
+	     fi             
+             if [ "$2" != 1 ]; then 
+	#	 sudo sed -i '/\[DMR Network 3\]/!b;n;cEnabled='"0"'' /etc/dmrgateway
+ 	         sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/dmr2nxdn 
 		 sudo sed  -i 's/62035/62031/g' /etc/mmdvmhost
 		 sudo sed  -i 's/127.0.0.3/tgif.network/g' /etc/mmdvmhost
 		 sudo sed  -i 's/none/passw0rd/g' /etc/mmdvmhost
              fi
- 	  sudo /usr/local/sbin/dmr2nsdn.service restart
- 	  sudo /usr/local/sbin/mmdvmhost.service restart
+# 	  sudo /usr/local/sbin/dmr2nxdn.service restart
+ #	  sudo /usr/local/sbin/mmdvmhost.service restart
 
         fi
 
-        if [ "$1" = 9 ]; then sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"$2"'' /etc/ysf2dmr
- 	  sudo /usr/local/sbin/ysf2dmr.service restart
-
+        if [ "$1" = 9 ]; then 
+                if [ "$2" = 1 ]; then
+ 			sudo sed -i '/\[DMR\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+			sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"1"'' /etc/ysf2dmr
+          		sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+          		sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+ 	  		sudo /usr/local/sbin/ysf2dmr.service restart
+		fi
+		if [ "$1" != 1 ]; then 
+			sudo sed -i '/\[DMR\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+                         sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/ysf2dmr
+                        sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+                        sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+		fi 
         fi
 
-        if [ "$1" = 10 ]; then sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"$2"'' /etc/ysf2nxdn
-          sudo /usr/local/sbin/ysf2nxdn.service restart
+        if [ "$1" = 10 ]; then 	
+                if [ "$2" = 1 ]; then
+	 		sudo sed -i '/\[DMR\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+			sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"1"'' /etc/ysf2nxdn
+                	sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+                	sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+			sudo /usr/local/sbin/ysf2nxdn.service restart
+                fi
+		if [ "$1" != 1 ]; then 
+	 		sudo sed -i '/\[DMR\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+                        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/ysf2nxdn
+                        sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+                        sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+                fi
 	fi
 
-        if [ "$1" = 11 ]; then sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"$2"'' /etc/ysf2p25
-          sudo /usr/local/sbin/ysf2p25.service restart 
+        if [ "$1" = 11 ]; then 
+ 		if [ "$2" = 1 ]; then 
+			sudo sed -i '/\[DMR\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+			sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"1"'' /etc/ysf2p25
+                	sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+                	sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+                        sudo /usr/local/sbin/ysf2p25.service restart
+                fi
+
+		if [ "$2" != 1 ]; then 
+			sudo sed -i '/\[DMR\]/!b;n;cEnable='"1"'' /etc/mmdvmhost
+                        sudo sed -i '/\[Enabled\]/!b;n;cEnabled='"0"'' /etc/ysf2p25
+                        sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+                        sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"0"'' /etc/mmdvmhost
+                 fi
+
 	fi
+   sleep 2 
+  sudo /usr/local/sbin/mmdvmhost.service restart
 
 fi;
