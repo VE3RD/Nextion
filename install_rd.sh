f8abbd03b648f584205196dcaa358eb3b78ef33a
root <root@pi-star>
2019-11-25 15:06:49 -0500

    File Cleanup Added Install.sh

---
+++ install_rd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+############################################################
+#  Set VE3RD Nextion Screen Install                        #
+#  VE3RD                                      2019/11/25   #
+############################################################
+set -o errexit
+set -o pipefail
+sudo mount -o remount,rw /
+echo "Drive set to RW"
+
+ddrive1=/usr/local/etc/
+
+ddrive2=/usr/local/etc/Nextion_Support/
+sudo cp NX*.tft $ddrive1 
+echo "   tft file coppied to $ddrive1"
+sudo cp *.sh $ddrive2		
+echo "   Script files copied to $ddrive2"
+
