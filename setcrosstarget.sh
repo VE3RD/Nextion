fdb0aca793e09c22c124cee72d2426df09f6d1a5
root <root@pi-star>
2019-11-28 23:15:42 -0500

    Added andv vrevised Script to write crossovers

---
+++ setcrosstarget.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+############################################################
+#  Set Cross Mode Targets                                  #
+#  VE3RD                                      2019-11-28   #
+############################################################
+set -o errexit
+set -o pipefail
+# Check all five cross modes and set each one to either 0 or 1
+#Clear all Main Modes
+sudo mount -o remount,rw /
+if [ -z "$2" ]; then
+    exit
+else
+
+    if [ "$1" = 1 ]; then 
+
+	sudo sed -i '/^\[/h;G;/DMR Network/s/\(DefaultDstTG=\).*/\1'"$2"'/m;P;d' /etc/dmr2ysf
+    fi
+ 
+    if [ "$1" = 2 ]; then 
+	sudo sed -i '/^\[/h;G;/Network/s/\(Startup=\).*/\1'"$2"'/m;P;d' /etc/nxdngateway
+    fi
+ 
+    if [ "$1" = 3 ]; then 
+	sudo sed -i '/^\[/h;G;/DMR Network/s/\(Address=\).*/\1'"$2"'/m;P;d' /etc/ysf2dmr
+    fi
+ 
+    if [ "$1" = 4 ]; then 
+	sudo sed -i '/^\[/h;G;/DMR Network/s/\(StartupDstId=\).*/\1'"$2"'/m;P;d' /etc/ysf2dmr
+    fi
+ 
+    if [ "$1" = 5 ]; then 
+	sudo sed -i '/^\[/h;G;/NXDN Network/s/\(StartupDstId=\).*/\1'"$2"'/m;P;d' /etc/ysf2nxdn
+    fi
+ 
+    if [ "$1" = 6 ]; then 
+#	sudo sed -i '/^\[/h;G;/NXDN Network/s/\(Id=\).*/\1'"$2"'/m;P;d' /etc/ysf2nxdn
+ 	sudo sed -i '/\[NXDN Network\]/!b;n;cId='"$2"'' /etc/ysf2nxdn    
+    fi
+ 
+    if [ "$1" = 7 ]; then 
+	sudo sed -i '/^\[/h;G;/P25 Network/s/\(StartupDstId=\).*/\1'"$2"'/m;P;d' /etc/ysf2p25
+
+     fi
+
+fi
+#sudo /usr/local/sbin/mmdvmhost.service restart  > /dev/null
+sudo mount -o remount,ro /
