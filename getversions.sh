474260af8bfa8aa1dce11da8911de0a610d1e9fa
root <root@pi-star>
2019-11-29 22:45:45 -0500

    Removed spaces around = signe to get rid of a special character

--- getversions.sh
+++ getversions.sh
@@ -15,18 +15,27 @@ exit
 fi
 
 if [ $1 = 1 ]; then 
-m1=$(sudo cat /etc/pistar-release | grep "\Pi-Star_Build_Date") 
-m2=$(sudo cat /etc/pistar-release | grep "\Version")
-m3=$(sudo cat /etc/pistar-release | grep "\ircddbgateway")  
+#m1=$(sudo cat /etc/pistar-release | grep "\Pi-Star_Build_Date") 
+#m2=$(sudo cat /etc/pistar-release | grep "\Version")
+#m3=$(sudo cat /etc/pistar-release | grep "\ircddbgateway")  
+m1=$(sudo sed -n '/^[^#]*Pi-Star_Build_Date/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+m2=$(sudo sed -n '/^[^#]*Version/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+m3=$(sudo sed -n '/^[^#]*ircddbgateway/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+
 mt="$m1|$m2|$m3"
 echo "$mt"
 fi
 
 if [ $1 = 4 ]; then
-m4=$(sudo cat /etc/pistar-release | grep "\dstarrepeater")  
-m5=$(sudo cat /etc/pistar-release | grep "\MMDVMHost")  
-m6=$(sudo cat /etc/pistar-release | grep "\kernel")
-m7=$(sudo cat /etc/pistar-release | grep "\Hardware")
+#m4=$(sudo cat /etc/pistar-release | grep "\dstarrepeater")  
+#m5=$(sudo cat /etc/pistar-release | grep "\MMDVMHost")  
+#m6=$(sudo cat /etc/pistar-release | grep "\kernel")
+#m7=$(sudo cat /etc/pistar-release | grep "\Hardware")
+m4=$(sudo sed -n '/^[^#]*dstarrepeater/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+m5=$(sudo sed -n '/^[^#]*MMDVMHost/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+m6=$(sudo sed -n '/^[^#]*kernel/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+m7=$(sudo sed -n '/^[^#]*Hardware/p' /etc/pistar-release | sed -E "s/[[:space:]]+//g")
+
 mt="$m4|$m5|$m6|$m7"
 echo "$mt"
 
@@ -34,14 +43,4 @@ echo "$mt"
 fi
 
 
-
-
-#[Pi-Star]
-#Pi-Star_Build_Date = 27-Aug-2019
-#Version = 4.1.0-RC6
-#ircddbgateway = 20181222
-#dstarrepeater = 20181222
-#MMDVMHost = 20191122_Pi-Star-v4
-#kernel = 4.19.75-v7+
-#Hardware = RPi
 
