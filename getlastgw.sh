ade90a6def685355c88c36fe04ed5392a8d2fa6c
root <root@pi-star>
2021-06-13 10:05:49 -0400

    updated DMR Display Screen Data

--- getlastgw.sh
+++ getlastgw.sh
@@ -8,6 +8,24 @@
 set -o errexit
 set -o pipefail
 
+Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
+        if [ $Addr = "127.0.0.1" ]; then
+                GW="ON"
+                #fn=$(ls /var/log/pi-star/DMRGateway* | tail -n1 )
+                NetType=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 4)
+                NetNum=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 6)
+		if [ "$NetType" = "RFRX" ]; then
+   			echo "GW RF Net: $NetNum"
+		else
+   			echo "GW RX Net: $NetNum"
+		fi
+        else
+                GW="OFF"
+                ms=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
+                echo "$ms"
+        fi
+
+
 #f1=$(ls -tr /var/log/pi-star/DMRG*)
 f1=$(ls  /var/log/pi-star/DMRGateway* | tail -n1)
 
@@ -18,34 +36,5 @@ NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
 #echo "$NetNum"
 
 #echo "$NetType: $NetNum"
-if [ "$NetType" = "RFRX" ]; then
-   echo "GW RF Net: $NetNum"
-else
-   echo "GW RX Net: $NetNum"
-fi
 
-
-exit
-
-tt1=$(sudo grep -i 'Network' "$f1" | tail -n5 | cut -d " " -f 7)
-echo "$tt1"
-
-if [ ! "$tt2" ]; then
- 	tt2=$(sudo grep -i 'RF' "$f1" | tail -n5 | cut -d " " -f9)
- 	echo "$tt2"
-fi
-exit
-tt0=$(echo "$tt" | cut -d' ' -f4)
-#echo "2:$tt0"
-
-if [ "$tt0" = "RF" ]; then
-	tt1=$(echo "$tt" | cut -d' ' -f6 | cut -d'=' -f2 | cut -d'_' -f1)
-	tt2=$(echo "$tt" | cut -d' ' -f6 | cut -d'=' -f2 | cut -d'_' -f2)
-	echo "R $tt1 $tt2" 
-fi
-if [ "$tt0" = "Network" ]; then
-	t1=$(echo "$tt" | cut -d' ' -f8 | cut -d'=' -f2 | cut -d'_' -f1)
-	t2=$(echo "$tt" | cut -d' ' -f8 | cut -d'=' -f2 | cut -d'_' -f2)
-	echo "N $t1 $t2"
-fi
 
--- getmaster.sh
+++ getmaster.sh
@@ -10,38 +10,17 @@ set -o errexit
 set -o pipefail
 
 
-#m1=$(sudo cat /etc/mmdvmhost | grep "\[DMR\]" -A 1 | grep "Enable=" | cut -b 8-9)
-#if [ $m1 = "0" ]; then
-#	echo "DMR OFF"
-#	exit
-#fi
-
 Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
 	if [ $Addr = "127.0.0.1" ]; then
 		GW="ON"
+		#fn=$(ls /var/log/pi-star/DMRGateway* | tail -n1 )
+		NetType=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 4)
+		NetNum=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 6)
+		echo "DMRGateway"
 	else
 		GW="OFF"
+		ms=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
+		echo "Master Server"
 	fi
-mtf=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
 
 
-if [ "$mtf" = 'DMRGateway' ]; then
-	f1=$(ls -tr /var/log/pi-star/DMRG*)
-	tt=$(grep Name= $f1 | tail -n1)
-	tt0=$(echo "$tt" | cut -d' ' -f4)
-
-	if [ "$tt0" = "RF" ]; then
-        	nt=$(echo "$tt" | cut -d' ' -f7 | cut -d'=' -f2)
-#        	echo "$nt"
-	fi
-	if [ "$tt0" = "Network" ]; then
-        	nt=$(echo "$tt" | cut -d' ' -f5)
-#        	echo "$nt"
-	fi
-out="DMRG Net $nt"
-else
-
-out="$mtf: : "
-fi
-#echo "$GW:$mtf:$nt"
-echo "$out"
