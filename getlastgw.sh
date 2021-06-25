2c2d8124e0f8262e965c0b9af9d8aad7f88ea2df
root <root@pi-star>
2021-06-25 18:03:04 -0400

    bug fix for RF User Display

--- getlastgw.sh
+++ getlastgw.sh
@@ -9,16 +9,20 @@ set -o errexit
 set -o pipefail
 
 Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/mmdvmhost)
+f1=$(ls  /var/log/pi-star/DMRGateway* | tail -n1)
+
         if [ $Addr = "127.0.0.1" ]; then
                 GW="ON"
                 #fn=$(ls /var/log/pi-star/DMRGateway* | tail -n1 )
-                NetType=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 4)
-                NetNum=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 6)
+                NetType=$(sudo tail -n1 "$f1" | cut -d " " -f 4)
+      #          NetNum=$(sudo tail -n1 /var/log/pi-star/DMRG* | cut -d " " -f 6)
+		NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
 		if [ "$NetType" = "RFRX" ]; then
-   			echo "GW RF Net: $NetNum"
+   			echo "GW RF Net ""${NetNum##*( )}"
 		else
-   			echo "GW RX Net: $NetNum"
+			echo "GW SV Net ""${NetNum##*( )}"
 		fi
+#echo "$NetType"
         else
                 GW="OFF"
                 ms=$(sudo sed -n '/^[^#]*'"$Addr"'/p' /usr/local/etc/DMR_Hosts.txt | sed -E "s/[[:space:]]+/|/g" | cut -d'|' -f1)
@@ -26,15 +30,5 @@ Addr=$(sed -nr "/^\[DMR Network\]/ { :l /^Address[ ]*=/ { s/.*=[ ]*//; p; q;}; n
         fi
 
 
-#f1=$(ls -tr /var/log/pi-star/DMRG*)
-f1=$(ls  /var/log/pi-star/DMRGateway* | tail -n1)
-
-#echo "$f1"
-NetType=$(sudo tail -n1 "$f1" | cut -d " " -f 4)
-#echo "$NetType"
-NetNum=$(sudo tail -n1 "$f1" | cut -d " " -f 6)
-#echo "$NetNum"
-
-#echo "$NetType: $NetNum"
 
 
